bf617a2ee4a41e2114cc3fb151be7d85cbe82285
marcos-hurst <marcos.adamczuk@hurst.capital>
2025-02-12 08:02:15 -0300

    move files + separate nvimcmp

--- init.lua
+++ init.lua
@@ -144,37 +144,7 @@ require("lazy").setup({
   },
   {
     "hrsh7th/nvim-cmp",
-    config = function()
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-        }),
-        sources = {
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-        },
-      })
-    end,
+    config = require('plugins.nvimcmp').setup
   },
   {
     "nvim-telescope/telescope.nvim",
---
+++ lua/plugins/nvimcmp.lua
@@ -0,0 +1,36 @@
+local M = {}
+
+function M.setup()
+  local cmp = require("cmp")
+  local luasnip = require("luasnip")
+
+  cmp.setup({
+    snippet = {
+      expand = function(args)
+        luasnip.lsp_expand(args.body)
+      end,
+    },
+    mapping = cmp.mapping.preset.insert({
+      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<CR>"] = cmp.mapping.confirm({ select = true }),
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+    }),
+    sources = {
+      { name = "nvim_lsp" },
+      { name = "luasnip" },
+    },
+  })
+end
+
+return M
+
